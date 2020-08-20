2f1267a91cf3a833ad8456d760aac2f0d6295a45
Weston Ganger <weston@westonganger.com>
2020-08-20 11:54:40 -0700

    git-er-done

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+### https://stackoverflow.com/a/24112741/3068360
+script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd $script_path
+
 ./setup.sh && bundle exec middleman deploy && bundle exec middleman sitemap_ping
